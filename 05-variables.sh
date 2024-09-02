97c5ea0dea63f9668b6b99e72a24f533aadbd4f1
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-02 21:18:47 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter username::"
+
+read -s USERNAME
+
+echo "username entered is: $USERNAME"
+
+read "please enter your password::"
+
+read -s PASSWORD
+
+echo "password entered is: $PASSWORD"

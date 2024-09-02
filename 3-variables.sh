cf8768ac362b1de6ada59324ef9a2d06ae219d1e
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-02 21:02:16 +0530

    variables

---
+++ 3-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=suresh
+
+echo "$PERSON1:: how are you $PERSON2 ?"
+echo "$PERSON2:: hello $PERSON1 i am good. how are you doing ?"
+echo "$PERSON1:: i am doing good $PERSON2 .ehat about you?"
+echo "$PERSON2:: i am learning shell scripting $PERSON1?"
+

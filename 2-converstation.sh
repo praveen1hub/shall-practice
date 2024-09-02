184eb284fb7527368049d5b8be8f858f28121884
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-02 20:47:36 +0530

    converstation

---
+++ 2-converstation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: how are you suresh ?"
+echo "suresh:: hello remesh i am good. how are you doing ?"
+echo "ramesh:: i am doing good suresh .ehat about you?"
+echo "suresh:: i am learning shell scripting ramesh?"

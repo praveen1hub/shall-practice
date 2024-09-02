b616ad0e028b55132de229fa73455d602ef1a90c
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-02 21:04:33 +0530

    variables

---
+++ 4-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: how are you $PERSON2 ?"
+echo "$PERSON2:: hello $PERSON1 i am good. how are you doing ?"
+echo "$PERSON1:: i am doing good $PERSON2 .ehat about you?"
+echo "$PERSON2:: i am learning shell scripting $PERSON1?"

3bd333de6a6dfc407363138dbdee19f5cfd09f94
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-03 23:42:17 +0530

    command

---
+++ 15-loop.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+
+check_ROOT(){
+    if [$USERID -ne 0]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+
+    fi
+}
+
+VALIDATE(){
+       
+       if [ $1 -ne o]
+       then 
+           echo -e "$2 is...FALIED"
+           exit 1
+        else
+           echo -e "$2 is...SUCCESS"
+        fi
+    }
+
+check_ROOT  
+
+for package in $@
+do
+
+  dnf list install $PACKAGE
+
+    if [ $? -ne 0]
+    then 
+
+      echo "$PACKAGE is not installed.going to install"
+      dnf install $PACKAGE -y
+      VALIDATE $? "installing $PACKAGE"
+    else
+
+      echo "$PACKAGE is already installed...nothing to do"
+    
+    fi
+done
+

36f226cca5abba3e09464ab3e4e4bb6d9125bde2
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-03 15:55:50 +0530

    command

--- 8-datatypes.sh
+++ 8-datatypes.sh
@@ -3,6 +3,6 @@
 NUMBER1=$1
 NUMBER2=$2
 
-sum =$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2))
 
-echo "The total of $NUMBER1 and $NUMBER2 is: $sum"
+echo "The total of $NUMBER1 and $NUMBER2 is: $SUM"

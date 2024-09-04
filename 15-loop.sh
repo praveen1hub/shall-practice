fc8c2f81cbaccef51253e52dedb02a17a54102aa
praveenlattala <146346859+praveenlattala@users.noreply.github.com>
2024-09-04 12:44:25 +0530

    command

--- 15-loop.sh
+++ 15-loop.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-N="\e[33m"
+N="\e[0m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -15,33 +15,32 @@ CHECK_ROOT(){
 }
 
 VALIDATE(){
-       
-       if [ $1 -ne o]
-       then 
-           echo -e "$2 is...FALIED"
-           exit 1
-        else
-           echo -e "$2 is...SUCCESS"
-        fi
-    }
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is... $G SUCCESS $N"
+    fi
+}
 
 CHECK_ROOT  
 
 for package in $@
 do
 
-  dnf list install $PACKAGE
-
+    dnf list install $PACKAGE
     if [ $? -ne 0 ]
     then 
 
-      echo "$PACKAGE is not installed.going to install"
-      dnf install $PACKAGE -y
-      VALIDATE $? "installing $PACKAGE"
+        echo "$PACKAGE is not installed.going to install"
+        dnf install $PACKAGE -y
+        VALIDATE $? "installing $PACKAGE"
     else
 
-      echo "$PACKAGE is already installed...nothing to do"
+        echo "$PACKAGE is already installed...nothing to do"
     
     fi
+
 done
 
